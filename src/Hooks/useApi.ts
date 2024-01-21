8db8a8475a2284d81ba02b865fcfdb5161abca7c
henhadayo1 <henhadayo@gmail.com>
2024-01-21 20:04:20 +0200

    Add useApi hook that handles all api calls

---
+++ src/Hooks/useApi.ts
@@ -0,0 +1,31 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import axios, { AxiosError, AxiosRequestConfig } from "axios";
+import { useEffect, useState } from "react";
+
+const useApi = <T>(url: string, config: AxiosRequestConfig) => {
+  const [data, setData] = useState<T | undefined>(undefined);
+
+  // TODO: Add loading and error states and export them out
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const { data } = await axios<T>(url, config);
+        setData(data);
+      } catch (error: unknown) {
+        let errorMessage = "Error has occurred ";
+        if (error instanceof AxiosError) {
+          errorMessage += error.response?.data?.message;
+        } else if (error instanceof Error) {
+          errorMessage += error.message;
+        }
+        console.log(errorMessage);
+      }
+    }
+    getData();
+  }, []);
+
+  return { data };
+};
+
+export default useApi;
