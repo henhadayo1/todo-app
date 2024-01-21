b9a2295fb9b8f39184617e7eb7ab203e1768e449
henhadayo1 <henhadayo@gmail.com>
2024-01-21 19:16:15 +0200

    Add add new todo view and functionality

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+import axios, { AxiosError } from "axios";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import Todo from "../Models/Todo";
 
 const Home: React.FC = () => {
-  return <h1>Home page</h1>;
+  const [input, setInput] = useState<string>("");
+
+  function submitEventHandler(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    addNewTodo();
+  }
+
+  async function addNewTodo() {
+    try {
+      const { data: todo } = await axios.post<Todo>(
+        "http://localhost:4000/todos",
+        {
+          text: input,
+        }
+      );
+
+      console.log(todo);
+    } catch (error: unknown) {
+      let errorMessage = "Error has occurred ";
+      if (error instanceof AxiosError) {
+        errorMessage += error.response?.data?.message;
+      } else if (error instanceof Error) {
+        errorMessage += error.message;
+      }
+      console.log(errorMessage);
+    }
+  }
+
+  return (
+    <>
+      <h1>Home page</h1>
+      <form onSubmit={submitEventHandler}>
+        <input
+          type="text"
+          placeholder="Add new todo"
+          value={input}
+          onChange={(event: ChangeEvent<HTMLInputElement>) =>
+            setInput(event.target.value)
+          }
+        />
+        <input type="submit" value="Add" />
+      </form>
+    </>
+  );
 };
 
 export default Home;
