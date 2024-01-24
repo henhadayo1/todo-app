4316504409ed2ea9dc55437dd1ecaf78c2ef5f6e
henhadayo1 <henhadayo@gmail.com>
2024-01-24 14:50:03 +0200

    Naming fixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
-import NotFound from "./Pages/NotFound";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
