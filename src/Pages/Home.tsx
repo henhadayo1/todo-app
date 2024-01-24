4ecd63cb90016523866371c42bc7197ccf271de5
henhadayo1 <henhadayo@gmail.com>
2024-01-24 14:49:47 +0200

    Use the useApi2 custom hook to fetch todos and add a todo

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,22 +1,26 @@
-import axios, { AxiosError } from "axios";
-import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
-import Todo from "../Models/Todo";
-import useApi from "../Hooks/useApi";
+import React, { ChangeEvent, FormEvent, useState } from "react";
+import Todo from "../models/Todo";
+import useApi2 from "../hooks/useApi2";
+import { AxiosRequestConfig } from "axios";
+
+const newTodoConfig: AxiosRequestConfig = {
+  method: "POST",
+};
 
 const Home: React.FC = () => {
   const [input, setInput] = useState<string>("");
-  const [todos, setTodos] = useState<Todo[]>([]);
 
-  const { data: fetchTodos } = useApi<Todo[]>("http://localhost:4000/todos", {
-    method: "GET",
-  });
+  const {
+    sendRequest: sendNewTodo,
+    isLoading: isSendNewTodoLoading,
+    errorMessage: sendNewTodoError,
+  } = useApi2<Todo>("http://localhost:4000/todos", newTodoConfig);
 
-  useEffect(() => {
-    console.log(fetchTodos);
-    if (fetchTodos) {
-      setTodos(fetchTodos);
-    }
-  }, [fetchTodos]);
+  const {
+    data: todos,
+    isLoading: isFetchTodosLoading,
+    errorMessage: fetchTodosError,
+  } = useApi2<Todo[]>("http://localhost:4000/todos");
 
   function submitEventHandler(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -24,25 +28,7 @@ const Home: React.FC = () => {
   }
 
   async function addNewTodo() {
-    try {
-      const { data: todo } = await axios.post<Todo>(
-        "http://localhost:4000/todos",
-        {
-          text: input,
-        }
-      );
-      setTodos([todo, ...todos]);
-
-      console.log(todo);
-    } catch (error: unknown) {
-      let errorMessage = "Error has occurred ";
-      if (error instanceof AxiosError) {
-        errorMessage += error.response?.data?.message;
-      } else if (error instanceof Error) {
-        errorMessage += error.message;
-      }
-      console.log(errorMessage);
-    }
+    sendNewTodo({ text: input });
   }
 
   return (
@@ -59,13 +45,15 @@ const Home: React.FC = () => {
           }
         />
         <input type="submit" value="Add" />
+        {isSendNewTodoLoading && <span>Sending data...</span>}
+        {sendNewTodoError && <span>{sendNewTodoError}</span>}
       </form>
 
       {/* Todo list */}
       <div>
-        {todos?.map((todo) => (
-          <div key={todo._id}>{todo.text}</div>
-        ))}
+        {isFetchTodosLoading && <span>Fetching todos...</span>}
+        {fetchTodosError && <span>{fetchTodosError}</span>}
+        {todos && todos.map((todo) => <div key={todo._id}>{todo.text}</div>)}
       </div>
     </>
   );
