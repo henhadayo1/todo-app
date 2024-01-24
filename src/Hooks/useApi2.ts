0677f87bbee4d2c34755264fa4ce1c1f1ead5ea2
henhadayo1 <henhadayo@gmail.com>
2024-01-24 14:45:34 +0200

    When useApi custom hook is a Get method than fetch data imediately when initialized

--- src/Hooks/useApi2.ts
+++ src/Hooks/useApi2.ts
@@ -1,33 +1,43 @@
 import axios, { AxiosError, AxiosRequestConfig } from "axios";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
-const useApi2 = <T>(url: string, config: AxiosRequestConfig) => {
-  const [data, setData] = useState<T | undefined>(undefined);
+const useApi2 = <T>(url: string, config?: AxiosRequestConfig) => {
+  const [data, setData] = useState<T | undefined>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string>("");
 
-  async function sendRequest(data?: unknown, params?: unknown) {
-    setIsLoading(true);
-    setErrorMessage("");
-    try {
-      const { data: fetchedData } = await axios(url, {
-        ...config,
-        data,
-        params,
-      });
-      setData(fetchedData);
-    } catch (error: unknown) {
-      let errorMessage = "Error has occurred ";
-      if (error instanceof AxiosError) {
-        errorMessage += error.response?.data?.message;
-      } else if (error instanceof Error) {
-        errorMessage += error.message;
+  const sendRequest = useCallback(
+    async function sendRequest(data?: unknown, params?: unknown) {
+      setIsLoading(true);
+      setErrorMessage("");
+      try {
+        console.log("Sending request to " + url);
+        const { data: fetchedData } = await axios(url, {
+          ...config,
+          data,
+          params,
+        });
+        setData(fetchedData);
+      } catch (error: unknown) {
+        let errorMessage = "Error has occurred ";
+        if (error instanceof AxiosError) {
+          errorMessage += error.response?.data?.message;
+        } else if (error instanceof Error) {
+          errorMessage += error.message;
+        }
+        setErrorMessage(errorMessage);
+        throw new Error(errorMessage);
       }
-      setErrorMessage(errorMessage);
-      console.log(errorMessage);
+      setIsLoading(false);
+    },
+    [config, url]
+  );
+
+  useEffect(() => {
+    if (!config || !config.method || config.method.toUpperCase() === "GET") {
+      sendRequest();
     }
-    setIsLoading(false);
-  }
+  }, [sendRequest, config]);
 
   return { sendRequest, data, isLoading, errorMessage };
 };
