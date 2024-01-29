13cb28a8b1929ad305e52647a0257fd8dfcd9cac
henhadayo1 <henhadayo@gmail.com>
2024-01-29 11:31:11 +0200

    Seperate the useAppDispatch and useAppSelector to hook file instead of store file

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todosReducer from "./todos/todosSlice";
-import { useDispatch, useSelector } from "react-redux";
 
 // export interface GenericState<T> {
 //   data?: T;
@@ -15,6 +14,3 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
-export const useAppSelector = useSelector.withTypes<RootState>();
