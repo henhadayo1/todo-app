8715479395cd47c71fd9b56dbfd66e802f9b8143
henhadayo1 <henhadayo@gmail.com>
2024-01-31 17:01:28 +0200

    Add button style component

---
+++ src/styles/button.styled.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  background: #bf4f74;
+  color: white;
+
+  font-size: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid #bf4f74;
+  border-radius: 3px;
+`;
+export default Button;
