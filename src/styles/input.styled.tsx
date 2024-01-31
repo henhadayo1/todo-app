0e501840dbdbdda798ac841af428e4769e630bc6
henhadayo1 <henhadayo@gmail.com>
2024-01-31 17:06:22 +0200

    Add input styled component

---
+++ src/styles/input.styled.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  color: #bf4f74;
+  font-size: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid #bf4f74;
+  border-radius: 3px;
+`;
+
+export default Input;
