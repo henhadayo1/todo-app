6cd7044f62f9a05bbd8a4bee67b9ac256e61cb24
henhadayo1 <henhadayo@gmail.com>
2024-02-05 15:59:51 +0200

    Add html5 for semantic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import { Link } from "react-router-dom";
+import todoLogo from "/public/assets/todo-logo.png";
+import Todos from "./pages/Todos";
 
 function App() {
   return (
     <>
+      <header>
+        <img src={todoLogo} alt="Todo logo" style={{ width: "3rem" }} />
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/todos">Todos</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/todos" element={<Todos />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <footer>Footer</footer>
     </>
   );
 }
