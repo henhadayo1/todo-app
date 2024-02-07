a5c07d9513bf83e02641c7c3e64ea26a659d401e
henhadayo1 <henhadayo@gmail.com>
2024-02-07 18:28:31 +0200

    Add add todo

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { FormEvent, useRef, useState } from "react";
 import TodoList from "../components/TodoList";
+import { useAddTodoMutation } from "../state/api/apiSlice";
 
 const Home: React.FC = () => {
+  const [isShowNewTodo, setIsShowNewTodo] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [addTodo, { isLoading }] = useAddTodoMutation();
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  async function submitEventHandler(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    await addTodo(inputValue);
+    inputRef.current?.blur();
+    setInputValue("");
+  }
+
   return (
     <div className="home_page-container">
       <header>
         <h2>Todo list</h2>
-        <button className="plus-button"></button>
+        <button
+          className="plus-button"
+          onClick={() => setIsShowNewTodo(true)}
+        ></button>
       </header>
       <main>
+        {isShowNewTodo && (
+          // TODO: Refactoring - reuse in TodoItem component as well
+          <form onSubmit={submitEventHandler}>
+            <input
+              type="text"
+              value={inputValue}
+              onChange={(event) => setInputValue(event.target.value)}
+              onBlur={() => setIsShowNewTodo(false)}
+              ref={inputRef}
+              autoFocus
+            />
+            {isLoading && <span>Adding todo...</span>}
+          </form>
+        )}
         <TodoList />
       </main>
     </div>
